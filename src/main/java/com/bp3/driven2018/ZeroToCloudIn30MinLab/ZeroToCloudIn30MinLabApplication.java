e52754d6b9e1d07387062095c05c417bf9f751fd
Sergii Tsymbal <stsymbal@bp-3.com>
2018-08-02 00:32:12 +0000

    Step 0: Imported Initializr project with server.port=0

---
+++ src/main/java/com/bp3/driven2018/ZeroToCloudIn30MinLab/ZeroToCloudIn30MinLabApplication.java
@@ -0,0 +1,12 @@
+package com.bp3.driven2018.ZeroToCloudIn30MinLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroToCloudIn30MinLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroToCloudIn30MinLabApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bp3/driven2018/ZeroToCloudIn30MinLab/ZeroToCloudIn30MinLabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bp3.driven2018.ZeroToCloudIn30MinLab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZeroToCloudIn30MinLabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
